bbcc936ef6797ef5ecefe04bcbf489e856e76f51
forwei <run.liuwei@live.com>
2017-01-14 17:30:27 +0800

    new

--- src/components/userlist.jsx
+++ src/components/userlist.jsx
@@ -1,6 +1,36 @@
 import React from 'react'
 import ReactDOM from "react-dom"
-import './userlist.css'
+
+class UserItem extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			hovered: false
+		}
+  }
+
+	handleMouseLeave(event) {
+		this.setState({hovered: false})
+	}
+
+	handleMouseEnter(event) {
+		this.setState({hovered: true})
+	}
+
+	render() {
+
+		const styles = getStyles(this.state)
+
+		return (
+			<div 
+			onMouseLeave={this.handleMouseLeave.bind(this)}
+			onMouseEnter={this.handleMouseEnter.bind(this)}
+			style={styles.item}>
+			<span>游客98206128</span></div>
+		)
+	}
+}
 
 export default class UserList extends React.Component{
 	constructor(props) {
@@ -8,23 +38,35 @@ export default class UserList extends React.Component{
 
   }
 	render() {
+
+		const styles = getStyles()
+
 		return (
-			<div className="userlist">
-				<div className="head">
+			<div id="userlist" style={styles.root}>
+				<div style={styles.headwrap}>
 					<span>在线会员</span>
 				</div>
-				<div className="tool">
-					<input type="text" id="usearch" />
+				<div>
+					<input type="text" style={styles.searchbtn} />
 					<span>在线</span>
 				</div>
-				<div className="listwrap">
-					<ul>
-						<li><span>游客98206128</span></li>
-						<li><span>游客98206128</span></li>
-						<li><span>游客98206128</span></li>
-						<li><span>游客98206128</span></li>
-						<li><span>游客98206128</span></li>
-					</ul>
+				<div>
+					<UserItem />
+					<UserItem />
+					<UserItem />
+					<UserItem />
+					<UserItem />
+					<UserItem />
+					<UserItem />
+					<UserItem />
+					<UserItem />
+					<UserItem />
+					<UserItem />
+					<UserItem />
+					<UserItem />
+					<UserItem />
+					<UserItem />
+					<UserItem />
 				</div>
 			</div>
 		)
@@ -32,4 +74,32 @@ export default class UserList extends React.Component{
 	componentDidMount() {
 
   }
+}
+
+function getStyles(state) {
+	return {
+		root: {
+			width: 250,
+			background: 'rgba(0, 0, 0, 0.2)',
+			color: '#fff'
+		},
+		item: {
+			cursor: 'pointer',
+			transition: 'background-color 300ms',
+			background: state && state.hovered ? 'rgba(0, 0, 0, 0.2)' : null
+		},
+		headwrap: {
+			borderBottom: 'rgba(255,255,255,0.3) 1px solid',
+			height: 31,
+			lineHeight: '31px',
+			paddingLeft: 5
+		},
+		searchbtn: {
+			border: 'none',
+			background: 'rgba(0,0,0,0.3)',
+			padding: '4px 5px',
+			marginLeft: '5px',
+			color: '#fff'
+		}
+	}
 }
