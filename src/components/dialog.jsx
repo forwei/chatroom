e19cddbd70d9b601a78fcd32e7dbae7f70742e3b
forwei <run.liuwei@live.com>
2017-02-12 23:46:47 +0800

    new

--- src/components/dialog.jsx
+++ src/components/dialog.jsx
@@ -5,12 +5,11 @@ export default class Dialog extends React.Component {
 
 	static propTypes = {
 		height: PropTypes.number.isRequired,
-		width: PropTypes.number.isRequired,
-		onClose: PropTypes.func.isRequired
+		width: PropTypes.number.isRequired
 	}
 
 	handleClose(e) {
-		this.props.onClose(e)
+		this.props.onClose && this.props.onClose(e)
 	}
 
 	stopClick(e) {
@@ -20,15 +19,15 @@ export default class Dialog extends React.Component {
 
 	render() {
 
-		let width = this.props.width + 16
-		let height = this.props.height + 16
+		let width = this.props.width
+		let height = this.props.height
 
 		return(
 			<div style={{content: '', clear: 'both', display: 'table'}}>
 			<RenderToLayer>
 			<div style={{position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, zIndex: 1000}} onClick={this.stopClick.bind(this)}>
 				<div style={{zIndex: 1020, position: 'absolute', width: width, height: height, top: '50%', left: '50%', marginTop: -height/2, marginLeft: -width/2, backgroundColor: '#fff', borderRadius: 3, boxShadow: 'rgba(0, 0, 0, 0.25) 0px 14px 45px, rgba(0, 0, 0, 0.2) 0px 10px 18px', color: '#333', overflow: 'hidden'}}>
-					<div style={{height: this.props.height, width: this.props.width, padding: 8}}>
+					<div style={{height: this.props.height, width: this.props.width}}>
 					{this.props.children}
 					</div>
 				</div>
--- src/components/infopanel.jsx
+++ src/components/infopanel.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import Skin from './skin'
+import Signin from './signin'
 
 
 class InfoPanel extends React.Component {
@@ -21,7 +22,8 @@ class InfoPanel extends React.Component {
 				</div>
 				<h4 style={{color: '#fff', textAlign: 'center'}}>{this.props.account.userLevel == 0 ? '游客' + this.props.account.name : this.props.account.name}</h4>
 				<div>
-					<Skin style={{color: '#fff', display: 'inline-block', cursor: 'pointer'}}>换肤</Skin>
+					<Skin style={{color: '#fff', display: 'inline-block', cursor: 'pointer', marginRight: 10}}>换肤</Skin>
+					<Signin style={{color: '#fff', display: 'inline-block', cursor: 'pointer'}}>登陆</Signin>
 				</div>
 			</div>
 		)
